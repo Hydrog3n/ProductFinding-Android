df576bcc6c6d7b5f8c4383faba0781eaf848b025
TristanProphete <tristan.prophete@gmail.com>
2016-03-24 15:44:52 +0100

    Add Login activity and Design

---
+++ app/src/main/java/com/example/alehmann/productfinding/LoginActivity.java
@@ -0,0 +1,29 @@
+package com.example.alehmann.productfinding;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText login_EditText;
+    EditText password_EditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        login_EditText = (EditText) findViewById(R.id.login_editText);
+        password_EditText = (EditText) findViewById(R.id.login_editText);
+    }
+
+    public void button_connexion(View button){
+      String login = login_EditText.getText().toString();
+      String password = login_EditText.getText().toString();
+    }
+
+    public void button_inscription(View button){
+
+    }
+}
