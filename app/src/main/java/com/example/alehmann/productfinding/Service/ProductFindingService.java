f929c8bd92a80055286f0d545d076bc1e6ce37f6
Amine.ben <kamine.ab@gmail.com>
2016-05-04 12:23:22 +0200

    post magasin

--- app/src/main/java/com/example/alehmann/productfinding/Service/ProductFindingService.java
+++ app/src/main/java/com/example/alehmann/productfinding/Service/ProductFindingService.java
@@ -6,10 +6,16 @@ import com.example.alehmann.productfinding.Classes.Magasin;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface ProductFindingService {
     @GET("magasin")
     Call<List<Magasin>> listMagasin();
+
+    @POST("magasin/")
+    Call<Magasin> createMagasin(@Body Magasin magasin);
+
 }
