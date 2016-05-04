30b0adda1373743c58a829de292fbdc16212091f
alehmann <alehmann70@gmail.com>
2016-05-04 16:31:31 +0200

    Commit Recycler Magasin

--- app/src/main/java/com/example/alehmann/productfinding/RecyclerAdapter.java
+++ app/src/main/java/com/example/alehmann/productfinding/RecyclerAdapter.java
@@ -1,7 +1,64 @@
 package com.example.alehmann.productfinding;
 
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 /**
  * Created by alehmann on 04/05/2016.
  */
-public class RecyclerAdapter {
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.CellHolder>{
+
+    private final Context _context;
+
+    public RecyclerAdapter(Context c) {
+        _context = c;
+    }
+
+    @Override
+    public CellHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View cell = LayoutInflater.from(_context).inflate(R.layout.cell,viewGroup,false);
+        return new CellHolder(cell);
+    }
+
+    @Override
+    public void onBindViewHolder(CellHolder cellHolder, int i) {
+        cellHolder.setData("Cellule "+i);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 20;
+    }
+
+    public class CellHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        private final TextView _cellLabel;
+        ImageView _image;
+        private String _data;
+
+        public CellHolder(View itemView) {
+            super(itemView);
+            _cellLabel = (TextView) itemView.findViewById(R.id.cell_text);
+            _cellLabel.setOnClickListener(this);
+            _image = (ImageView)itemView.findViewById(R.id.cell_image);
+        }
+
+        public void setData(String t){
+            _cellLabel.setText(t);
+            _data = t;
+        }
+
+        @Override
+        public void onClick(View view) {
+            Intent detailIntent = new Intent(_context,DetailMagasinActivity.class);
+            detailIntent.putExtra(DetailMagasinActivity.DETAIL_TEXT_KEY, _data);
+            _context.startActivity(detailIntent);
+        }
+    }
 }
--- app/src/main/java/com/example/alehmann/productfinding/RecyclerMagasinActivity.java
+++ app/src/main/java/com/example/alehmann/productfinding/RecyclerMagasinActivity.java
@@ -2,6 +2,8 @@ package com.example.alehmann.productfinding;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class RecyclerMagasinActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class RecyclerMagasinActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_magasin);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView.setAdapter(new RecyclerAdapter(this));
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
