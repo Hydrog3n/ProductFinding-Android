2b9eedf8b487ca40d6c0dca4ed67c1654f3ebf3b
alehmann <alehmann70@gmail.com>
2016-06-22 12:16:03 +0200

    Sort produit in detailMagasin

--- app/src/main/java/com/example/alehmann/productfinding/Magasin/DetailMagasinActivity.java
+++ app/src/main/java/com/example/alehmann/productfinding/Magasin/DetailMagasinActivity.java
@@ -6,9 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,6 +25,8 @@ import com.example.alehmann.productfinding.Service.Service;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import retrofit2.Call;
@@ -37,10 +42,12 @@ public class DetailMagasinActivity extends AppCompatActivity {
     private TextView villeMagTxV;
     private TextView addressMagTxV;
     private ImageView imageMagasinView;
+    private EditText rechercheProduit;
 
     private List<Produit> _produits;
 
     private RecyclerView recyclerViewProduits;
+    private ArrayList<Produit> _filtredProduit;
 
     @Override
     public void onResume() {
@@ -52,6 +59,7 @@ public class DetailMagasinActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //UI Content
+
         _context = getApplicationContext();
 
         setContentView(R.layout.activity_detail_magasin);
@@ -61,6 +69,30 @@ public class DetailMagasinActivity extends AppCompatActivity {
         villeMagTxV = (TextView) findViewById(R.id.text_view_Mag_Ville);
         addressMagTxV = (TextView) findViewById(R.id.text_view_Mag_Address);
         imageMagasinView = (ImageView) findViewById(R.id.image_magasin_view);
+        rechercheProduit = (EditText) findViewById(R.id.recherche_produit_magasin);
+
+        rechercheProduit.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                _filtredProduit = new ArrayList<Produit>();
+                for(Produit p : _produits) {
+                    if (p.getDescriptif().contains(s))
+                        _filtredProduit.add(p);
+                }
+                updateProduitList(_filtredProduit);
+            }
+        });
 
         mag = (Magasin) this.getIntent().getExtras().getSerializable("mag");
 
@@ -75,13 +107,16 @@ public class DetailMagasinActivity extends AppCompatActivity {
         recyclerViewProduits = (RecyclerView) findViewById(R.id.recycler_produit_view);
         recyclerViewProduits.setAdapter(new RecyclerProduitAdapter(this, _produits));
         recyclerViewProduits.setLayoutManager(new LinearLayoutManager(this));
-        //updateProduitData();
 
+        if (mag.getLogoUrl() != null)
+            Picasso.with(this).load(mag.getLogoUrl()).into(imageMagasinView);
+        else
+            imageMagasinView.setImageBitmap(null);
     }
 
-    public void updateProduitList() {
+    public void updateProduitList(List<Produit> produits) {
         RecyclerProduitAdapter rpa = (RecyclerProduitAdapter) recyclerViewProduits.getAdapter();
-        rpa.setProduits(_produits);
+        rpa.setProduits(produits);
     }
 
     @Override
@@ -98,7 +133,6 @@ public class DetailMagasinActivity extends AppCompatActivity {
                 r.putExtra("mag", mag);
                 startActivity(r);
 
-
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -114,7 +148,7 @@ public class DetailMagasinActivity extends AppCompatActivity {
             public void onResponse(Call<List<Produit>> call, Response<List<Produit>> response) {
                 if (response.isSuccessful()) {
                     _produits = response.body();
-                    updateProduitList();
+                    updateProduitList(_produits);
                 }
             }
 
@@ -125,5 +159,4 @@ public class DetailMagasinActivity extends AppCompatActivity {
             }
         });
     }
-
 }
